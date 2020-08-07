8cadbae7fe8832fe6c42d9cb0f1565f13e383bcb
Joao Pedro <pedrobarros2010@gmail.com>
2020-08-07 17:09:54 -0300

    adding cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,11 @@
 import express from 'express'
 import routes from './routes'
+import cors from 'cors'
 
 const app = express()
 
 app.use(express.json())
 app.use(routes)
+app.use(cors())
 
 app.listen(3030, () => console.log('Server is running on http://localhost:3030'))
