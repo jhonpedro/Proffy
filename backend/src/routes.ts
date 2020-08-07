c2da27775713f1f98a90bc3d3ba6e043051fd6bb
Joao Pedro <pedrobarros2010@gmail.com>
2020-08-07 17:13:31 -0300

    ending day 2

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -2,7 +2,12 @@ import express from 'express'
 const routes = express.Router()
 
 import classesControler from './controllers/ClassesControler'
+import connectionsController from './controllers/ConnectionsController'
 
+routes.get('/classes', classesControler.index)
 routes.post('/classes', classesControler.create)
+
+routes.post('/connections', connectionsController.create)
+routes.get('/connections', connectionsController.index)
 
 export default routes
