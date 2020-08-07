a155082386df34b8545cc84d149bb92450cfa746
Joao Pedro <pedrobarros2010@gmail.com>
2020-08-07 15:02:22 -0300

    adding configs and fixing .gitignore

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,9 @@
 import express from 'express'
+import routes from './routes'
 
 const app = express()
 
 app.use(express.json())
-
-app.get('/test', (req, res) => {
-  res.json({ test: "ok!" })
-})
+app.use(routes)
 
 app.listen(3030, () => console.log('Server is running on http://localhost:3030'))
