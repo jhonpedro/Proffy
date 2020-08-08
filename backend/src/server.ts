2ca0987f7d9907cf9a6c1fe465cce00b3d70a47a
Joao Pedro <pedrobarros2010@gmail.com>
2020-08-08 16:21:33 -0300

    testing queryes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,8 +4,8 @@ import cors from 'cors'
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(routes)
-app.use(cors())
 
 app.listen(3030, () => console.log('Server is running on http://localhost:3030'))
