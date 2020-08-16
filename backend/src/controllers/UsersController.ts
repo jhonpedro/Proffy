245755358dd9169937bff1f492575f57115e6a99
Joao Pedro <pedrobarros2010@gmail.com>
2020-08-16 16:01:19 -0300

    creating create user method

--- backend/src/controllers/UsersController.ts
+++ backend/src/controllers/UsersController.ts
@@ -3,10 +3,22 @@ import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 
 import { emailValidator } from '../utils'
-import { findUserByEmailRepository } from '../repositories'
-
+import { findUserByEmailRepository, createUserRepository } from '../repositories'
 
 export default {
+  async create(req: Request, res: Response) {
+    const { name, email, password } = req.body
+
+    const isEmailValid = emailValidator(email)
+
+    if (!isEmailValid || !name || !password) {
+      return res.status(401)
+    }
+
+    const createdUser = await createUserRepository({ email, name, password })
+
+    return res.status(200).json(createdUser)
+  },
   async authenticate(req: Request, res: Response) {
     const { email, password } = req.body
 
