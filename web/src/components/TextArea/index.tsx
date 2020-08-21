e2cf578785a28798c4bea228bfaae446c2bcc6f0
Joao Pedro <pedrobarros2010@gmail.com>
2020-08-21 20:10:55 -0300

    moving TextArea from css to styled-components

--- web/src/components/TextArea/index.tsx
+++ web/src/components/TextArea/index.tsx
@@ -1,6 +1,11 @@
 import React, { TextareaHTMLAttributes } from 'react'
 
-import './styles.css'
+// import './styles.css'
+
+import {
+  TextAreaContainer,
+  TextArea
+} from './styles'
 
 interface TextareaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {
   label: string,
@@ -9,10 +14,10 @@ interface TextareaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {
 
 const Textarea: React.FC<TextareaProps> = ({ label, name, ...rest }) => {
   return (
-    <div className="textarea-block">
+    <TextAreaContainer>
       <label htmlFor={name}>{label}</label>
-      <textarea {...rest} id={name} />
-    </div>
+      <TextArea {...rest} id={name} />
+    </TextAreaContainer>
   )
 }
 
