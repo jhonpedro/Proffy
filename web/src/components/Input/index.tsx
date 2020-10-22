ab10cbd4637951b4b94571dc22b81e1b05c22fd3
Joao Pedro <pedrobarros2010@gmail.com>
2020-10-22 16:05:35 -0300

    creating a new layout for input

--- web/src/components/Input/index.tsx
+++ web/src/components/Input/index.tsx
@@ -1,24 +1,31 @@
-import React, { InputHTMLAttributes } from 'react'
+import React, { InputHTMLAttributes, useState, FocusEvent } from 'react'
 
-import {
-  InputBlock,
-  InputElement
-} from './styles'
+import { InputBlock, InputElement, Label } from './styles'
 
 // import './styles.css'
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
-  label: string,
-  name: string
+	label: string
+	name: string
 }
 
 const Input: React.FC<InputProps> = ({ label, name, ...rest }) => {
-  return (
-    <InputBlock>
-      <label htmlFor={name}>{label}</label>
-      <InputElement type="text" {...rest} id={name} />
-    </InputBlock>
-  )
+	const [labelAbove, setLabelAbove] = useState(false)
+
+	function handleFocus(event: FocusEvent<HTMLInputElement>) {
+		if (event.target.value === '') {
+			setLabelAbove(false)
+			return
+		}
+		setLabelAbove(true)
+	}
+
+	return (
+		<InputBlock>
+			<Label isUp={labelAbove}>{label}</Label>
+			<InputElement onBlur={handleFocus} type="text" {...rest} id={name} />
+		</InputBlock>
+	)
 }
 
 export default Input
--- web/src/components/Input/styles.ts
+++ web/src/components/Input/styles.ts
@@ -1,40 +1,57 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+
+interface WrapperProps {
+	isUp?: boolean
+}
 
 export const InputBlock = styled.div`
-  position: relative;
+	position: relative;
+	overflow: hidden;
 
-  + .input-block {
-    margin-top: 1.4rem;
-  }
+	:focus-within::after {
+		height: calc(100% - 2rem);
+		width: 3px;
+		content: '';
+		background: var(--color-primary-light);
+		position: absolute;
+		left: -1px;
+		top: 1rem;
+		bottom: 1rem;
+		border-radius: 1rem;
+	}
 
-  label {
-    font-size: 1.4rem;
-  }
+	:focus-within label {
+		font-size: 1rem;
+		top: 0.6rem;
+	}
+`
 
-  
-  :focus-within::after {
-    width: calc(100% - 3.2rem);
-    height: 2px;
-    content: '';
-    background: var(--color-primary-light);
-    position: absolute;
-    left: 1.6rem;
-    right: 1.6rem;
-    bottom: 0;
-    border-radius: 0.8rem;
-  }
+export const Label = styled.label<WrapperProps>`
+	position: absolute;
+	left: 1.6rem;
 
-  
+	transition: 0.5s ease;
+	${(props) => {
+		if (props.isUp) {
+			return css`
+				top: 0.6rem;
+				font-size: 1rem;
+			`
+		} else {
+			return css`
+				top: 1.6rem;
+			`
+		}
+	}}
 `
 
 export const InputElement = styled.input`
-  width: 100%;
-  height: 5.6rem;
-  margin-top: 0.8rem;
-  border-radius: 0.8rem;
-  background: var(--color-input-background);
-  border: 1px solid var(--color-line-in-white);
-  outline: 0;
-  padding: 0 1.6rem;
-  font: 1.6rem Archivo;
+	width: 100%;
+	height: 5.6rem;
+	border-radius: 0.8rem;
+	background: var(--color-input-background);
+	border: 1px solid var(--color-line-in-white);
+	outline: 0;
+	padding: 0 1.6rem;
+	font: 1.6rem Archivo;
 `
