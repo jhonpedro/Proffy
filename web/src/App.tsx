0c4521c15020e7b76751d0e1e46d2ede9323a44f
Joao Pedro <pedrobarros2010@gmail.com>
2020-11-03 20:56:49 -0300

    adding react-toastify container

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,16 +1,21 @@
 import React from 'react'
 import GlobalStyle from './assets/styles/global'
+import { ToastContainer } from 'react-toastify'
+
+import 'react-toastify/dist/ReactToastify.min.css'
+
 // import './assets/styles/global.css'
 
 import Routes from './routes'
 
 function App() {
-  return (
-    <React.Fragment>
-      <GlobalStyle />
-      <Routes />
-    </React.Fragment>
-  )
+	return (
+		<React.Fragment>
+			<GlobalStyle />
+			<Routes />
+			<ToastContainer />
+		</React.Fragment>
+	)
 }
 
 export default App
