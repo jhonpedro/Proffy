d1800c2ce0afcf6a4ca5cdf41c85858ab00b6233
Joao Pedro <pedrobarros2010@gmail.com>
2020-11-05 14:37:43 -0300

    formating large lines

--- backend/src/repositories/forgotPassword_token/removeForgotPassword_token.ts
+++ backend/src/repositories/forgotPassword_token/removeForgotPassword_token.ts
@@ -1,9 +1,11 @@
-import Database from "../../database/connection";
+import Database from '../../database/connection'
 interface removeForgotPassword_tokenRepositoryProps {
-  user_id: number
+	user_id: number
 }
-export default async function removeForgotPassword_tokenRepository({ user_id }: removeForgotPassword_tokenRepositoryProps) {
-  await Database('forgotPassword_token').delete().where({ user_id })
+export default async function removeForgotPassword_tokenRepository({
+	user_id,
+}: removeForgotPassword_tokenRepositoryProps) {
+	await Database('forgotPassword_token').delete().where({ user_id })
 
-  return true
+	return true
 }
