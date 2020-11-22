b845eeeddb2fe70ec162079b7c0c78f557e77db0
Joao Pedro <pedrobarros2010@gmail.com>
2020-11-22 19:53:34 -0300

    refactoring to new methods

--- backend/src/controllers/ClassesControler.ts
+++ backend/src/controllers/ClassesControler.ts
@@ -2,89 +2,56 @@ import { Request, Response } from 'express'
 
 import db from '../database/connection'
 import { convertHourToMinutes } from '../utils/helpers/'
-
-interface ScheduleItem {
-	week_day: number
-	from: string
-	to: string
-}
+import { createClass, getBySubjectOrTime } from '../repositories/classes'
 
 export default {
 	async index(req: Request, res: Response) {
 		const filters = req.query
 
-		const week_day = filters.week_day as string
 		const subject = filters.subject as string
-		const time = filters.time as string
+		const start = filters.start as string
+		const end = filters.end as string
+		try {
+			const startInMinutes = start ? convertHourToMinutes(start) : undefined
+			const endInMinutes = end ? convertHourToMinutes(end) : undefined
 
-		if (!week_day || !subject || !time) {
-			return res.status(400).json({ error: 'Invalid params to search classes' })
-		}
-
-		const timeInMinutes = convertHourToMinutes(time)
-
-		const classes = await db('classes')
-			.whereExists(function () {
-				this.select('class_schedule.*')
-					.from('class_schedule')
-					.whereRaw('`class_schedule`.`class_id` = `classes`.`id`')
-					.whereRaw('`class_schedule`.`week_day` = ??', [Number(week_day)])
-					.whereRaw('`class_schedule`.`from` <= ??', [timeInMinutes])
-					.whereRaw('`class_schedule`.`to` > ??', [timeInMinutes])
+			const classes = await getBySubjectOrTime({
+				subject,
+				start: startInMinutes,
+				end: endInMinutes,
 			})
-			.where('classes.subject', '=', subject)
-			.join('users', 'classes.user_id', '=', 'users.id')
-			.select(['users.*', 'classes.*'])
 
-		return res.json(classes)
+			return res.json(classes)
+		} catch (error) {
+			return res.status(500).json({ message: error.message })
+		}
 	},
 
 	async create(req: Request, res: Response) {
-		const { name, avatar, whatsapp, bio, subject, cost, schedule } = req.body
+		const { biography, subject, cost, schedule } = req.body
 
-		if (!name || !whatsapp || !bio || !subject || !cost || !schedule) {
+		if (!biography || !subject || !cost || !schedule) {
 			return res.status(400).json({ error: 'Missing params' })
 		}
 
-		const trx = await db.transaction()
+		const user_id = req.user.id
 
 		try {
-			const insertedUserIds = await trx('users').insert({
-				name,
-				avatar,
-				whatsapp,
-				bio,
-			})
-
-			const user_id = insertedUserIds[0]
-
-			const insertedClassesId = await trx('classes').insert({
+			const wasClassSavedSuccesfully = await createClass({
+				user_id,
+				biography,
 				subject,
 				cost,
-				user_id,
+				schedule,
 			})
 
-			const class_id = insertedClassesId[0]
-
-			const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
-				return {
-					class_id,
-					week_day: scheduleItem.week_day,
-					from: convertHourToMinutes(scheduleItem.from),
-					to: convertHourToMinutes(scheduleItem.to),
-				}
-			})
-
-			await trx('class_schedule').insert(classSchedule)
-
-			await trx.commit()
+			if (!wasClassSavedSuccesfully) {
+				throw new Error('error in class creation')
+			}
 
 			return res.sendStatus(201)
 		} catch (error) {
-			await trx.rollback()
-			return res
-				.status(400)
-				.json({ error: error.message, where: 'In route of class creation' })
+			return res.status(400).json({ error: error.message })
 		}
 	},
 }
