ded04d7d214496b7dbcd2fcb2afd3065341ab9d5
Joao Pedro <pedrobarros2010@gmail.com>
2020-11-23 12:57:36 -0300

    defining function return and selecting columns

--- backend/src/repositories/User/findUserByEmail.ts
+++ backend/src/repositories/User/findUserByEmail.ts
@@ -1,7 +1,26 @@
 import Database from '../../database/connection'
 
-export default async function findUserByEmailRepository(email: string) {
-	const user = await Database('users').select().where({ email }).first()
+export default async function findUserByEmailRepository(
+	email: string
+): Promise<{
+	id: number
+	name: string
+	last_name: string
+	password: string
+	whatsapp: string
+	photo: string
+}> {
+	const user = await Database('users as u')
+		.select([
+			'u.name',
+			'u.last_name',
+			'u.password',
+			'u.whatsapp',
+			'u.photo',
+			'u.id',
+		])
+		.where({ email })
+		.first()
 
 	return user
 }
