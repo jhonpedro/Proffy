c609f10b58aad2dce86004e77b0a3c376b9bc621
Joao Pedro <pedrobarros2010@gmail.com>
2020-11-29 16:58:27 -0300

    creating auth hook

---
+++ web/src/hooks/auth.tsx
@@ -0,0 +1,111 @@
+import React, { createContext, useContext, useState } from 'react'
+import axios from '../services/axios'
+
+interface User {
+	id: number
+	name: string
+	last_name: string
+	photo: string
+	whatsappp: string
+}
+
+interface AuthContextData {
+	token: string
+	singIn(email: string, password: string, remember: boolean): void
+	getUser(): User | undefined
+}
+
+interface SessionPostProps {
+	token: string
+	user: User
+}
+
+const AuthContext = createContext<AuthContextData>({} as AuthContextData)
+
+const AuthProvider: React.FC = ({ children }) => {
+	const [user, setUser] = useState<User>(() => {
+		const localResponse = localStorage.getItem('user')
+		if (!localResponse) {
+			return
+		}
+
+		const user = JSON.parse(localResponse)
+
+		return user
+	})
+	const [token, setToken] = useState<string>(() => {
+		const localResponse = localStorage.getItem('token') as string
+		if (!localResponse) {
+			return ''
+		}
+		axios.defaults.headers.Authorization = `Bearer ${localResponse}`
+		return localResponse
+	})
+	const [remember, setRemeber] = useState(() => {
+		const remember = localStorage.getItem('remember')
+
+		if (!remember) {
+			return false
+		} else {
+			return true
+		}
+	})
+
+	async function singIn(email: string, password: string, remember: boolean) {
+		console.log('a')
+		const response = await axios.post<SessionPostProps>('/user/session', {
+			email,
+			password,
+		})
+
+		const { token, user } = response.data
+
+		axios.defaults.headers.Authorization = `Bearer ${token}`
+
+		setToken(token)
+		setUser(user)
+		if (remember) {
+			setRemeber(true)
+			localStorage.setItem('user', JSON.stringify(user))
+			localStorage.setItem('token', token)
+		}
+	}
+
+	function getUser() {
+		if (!remember) {
+			return user
+		}
+
+		const localResponse = localStorage.getItem('user') as string
+		if (!localStorage) {
+			return undefined
+		}
+		const userParsed = JSON.parse(localResponse)
+
+		return userParsed
+	}
+
+	return (
+		<AuthContext.Provider
+			value={{
+				singIn,
+				getUser,
+				token,
+			}}
+		>
+			{children}
+		</AuthContext.Provider>
+	)
+}
+
+function useAuth() {
+	const context = useContext(AuthContext)
+
+	if (!context) {
+		throw new Error('useAuth must be used within a AuthProvider')
+	}
+
+	return context
+}
+
+export { AuthProvider, useAuth }
