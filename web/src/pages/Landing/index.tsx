f331f7d6de94e0195d60c3be565c8669150fc15b
João Pedro <pedrobarros2010@gmail.com>
2020-12-03 21:15:37 -0300

    creating a header with user photo

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
-import api from '../../services/api'
+import { useHistory } from 'react-router-dom'
+import {FaLaugh} from 'react-icons/fa'
+import axios from '../../services/axios'
 
+import { User, useAuth } from '../../hooks/auth'
 import logoImg from '../../assets/images/logo.svg'
 import landingImg from '../../assets/images/landing.svg'
-
 import studyIcon from '../../assets/images/icons/study.svg'
 import giveClasIcons from '../../assets/images/icons/give-classes.svg'
 import purpleHeart from '../../assets/images/icons/purple-heart.svg'
@@ -12,57 +14,83 @@ import purpleHeart from '../../assets/images/icons/purple-heart.svg'
 // import './styles.css'
 
 import {
-  PageLanding,
-  PageLandingContent,
-  LogoContainer,
-  ButtonsContainer,
-  Button,
-  Span
+	PageLanding,
+	Header,
+	PageLandingContent,
+	LogoContainer,
+	ButtonsContainer,
+	Button,
+	Span,
 } from './styles'
 
 function Landing() {
-  const [totalConnections, setTotalConnections] = useState(0)
+	const { push } = useHistory()
+	const {getUser} = useAuth()
+	const [totalConnections, setTotalConnections] = useState(0)
+	const [user] = useState<User>(() => {
+		const userLocalstorage = getUser()
 
-  useEffect(() => {
-    api.get('/connections').then((res: any) => {
-      const { total } = res.data
-      setTotalConnections(total)
-    })
-  }, [totalConnections])
+		if(!userLocalstorage){
+			push('/sing-in')
+			return {} as User
+		}
 
-  return (
-    <PageLanding>
-      <PageLandingContent className="container">
-        <LogoContainer>
-          <img src={logoImg} alt="Proffy" />
-          <h2>Sua plataforma de estudos online</h2>
-        </LogoContainer>
-        <img
-          src={landingImg}
-          alt="Plataforma de estudos"
-          className="hero-image"
-        />
-        <ButtonsContainer>
-          <Button study>
-            <Link to="/study">
-              <img src={studyIcon} alt="Estudar" />
-              Estudar
-            </Link>
-          </Button>
-          <Button give_classes>
-            <Link to="/give-classes">
-              <img src={giveClasIcons} alt="Dar aulas" />
-              Dar aulas
-            </Link>
-          </Button>
+		return userLocalstorage
+	})
 
-        </ButtonsContainer>
-        <Span className="total-connections">
-          Total de {totalConnections} conexões já realizadas <img src={purpleHeart} alt="Coração roxo" />
-        </Span>
-      </PageLandingContent>
-    </PageLanding>
-  )
+	useEffect(() => {
+		axios.get('/connections').then((res: any) => {
+			const { total } = res.data
+			setTotalConnections(total)
+		})
+	}, [totalConnections, user])
+
+	return (
+		<PageLanding>
+			<PageLandingContent className="container">
+					{user ? (
+						<Header>
+						<div className="user">
+							{user.photo ? (
+								<img src={user.photo} alt={`Foto de ${user.name}`} />
+								) : (
+									<FaLaugh size="4rem"/>
+							)}
+							<span>{user.name + ' ' + user.last_name}</span>
+						</div>
+						<div className="hamburger"></div>
+				</Header>
+					) : null}
+				<LogoContainer>
+					<img src={logoImg} alt="Proffy" />
+					<h2>Sua plataforma de estudos online</h2>
+				</LogoContainer>
+				<img
+					src={landingImg}
+					alt="Plataforma de estudos"
+					className="hero-image"
+				/>
+				<ButtonsContainer>
+					<Button study>
+						<Link to="/study">
+							<img src={studyIcon} alt="Estudar" />
+							Estudar
+						</Link>
+					</Button>
+					<Button give_classes>
+						<Link to="/give-classes">
+							<img src={giveClasIcons} alt="Dar aulas" />
+							Dar aulas
+						</Link>
+					</Button>
+				</ButtonsContainer>
+				<Span className="total-connections">
+					Total de {totalConnections} conexões já realizadas{' '}
+					<img src={purpleHeart} alt="Coração roxo" />
+				</Span>
+			</PageLandingContent>
+		</PageLanding>
+	)
 }
 
 export default Landing
--- web/src/pages/Landing/styles.ts
+++ web/src/pages/Landing/styles.ts
@@ -1,161 +1,228 @@
 import styled, { css } from 'styled-components'
 
 export const PageLanding = styled.div`
-  width: 100vw;
-  height: 100vh;
+	width: 100vw;
+	height: 100vh;
 
-  display: flex;
-  justify-content: center;
-  align-items: center;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
 
-  color: var(--color-text-in-primary);
-  background: var(--color-primary);
+	color: var(--color-text-in-primary);
+	background: var(--color-primary);
 `
 
 export const PageLandingContent = styled.div`
-  > img {
-    width: 95%;
-  }
+	> img {
+		width: 60%;
+		@media (min-width: 1100px) {
+			width: 80%;
+		}
+	}
+	height: 100%;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	position: relative;
 
+	@media (min-width: 1100px) {
+		max-width: 1100px;
+		display: grid;
+		grid-template-rows: 350px 1fr;
+		grid-template-columns: 2fr 1fr 1fr;
+		grid-template-areas:
+			'logo hero hero'
+			'buttons buttons total';
 
-  @media (min-width: 1100px) {
-    max-width: 1100px;
-    display: grid;
-    grid-template-rows: 350px 1fr;
-    grid-template-columns: 2fr 1fr 1fr;
-    grid-template-areas:
-      "logo hero hero"
-      "buttons buttons total"
-    ;
+		> img {
+			grid-area: hero;
+		}
+	}
+`
 
-    > img {
-      grid-area: hero;
-    }
-  }
-  
+export const Header = styled.header`
+	width: 100%;
+	height: 10%;
+	padding: 0 2rem;
+	border: 1px solid red;
+	position: absolute;
+	top: 0;
+
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+
+	.hamburger {
+		position: relative;
+		width: 2rem;
+		height: 2px;
+		border-radius: 1px;
+		background-color: white;
+
+		:after {
+			width: 2rem;
+			content: '';
+			height: 2px;
+			border-radius: 1px;
+			background-color: white;
+			position: absolute;
+			top: -5px;
+		}
+		:before {
+			width: 2rem;
+			content: '';
+			height: 2px;
+			border-radius: 1px;
+			background-color: white;
+			position: absolute;
+			bottom: -5px;
+		}
+	}
+
+	.user {
+		width: 50%;
+
+		display: flex;
+		align-items: center;
+
+		span {
+			margin-left: 1rem;
+		}
+
+		img {
+			border-radius: 100%;
+			width: 4rem;
+		}
+	}
 `
 
 export const LogoContainer = styled.div`
-  text-align: center;
+	text-align: center;
+	margin-top: 2.5rem;
 
-  img {
-    height: 10rem;
-  }
+	img {
+		height: 6rem;
+		@media (min-width: 1100px) {
+			height: 10rem;
+		}
+	}
 
-  h2 {
-    font-weight: 500;
-    font-size: 2.4rem;
-    line-height: 4.6rem;
-    margin-top: 0.8rem;
-  }
+	h2 {
+		font-weight: 500;
+		font-size: 1.5rem;
+		@media (min-width: 1100px) {
+			line-height: 4.6rem;
+		}
+		margin-top: 0.5rem;
+	}
 
-  @media (min-width: 1100px) {
-    grid-area: logo;
-    align-self: center;
-    text-align: left;
-    margin: 0;
+	@media (min-width: 1100px) {
+		grid-area: logo;
+		align-self: center;
+		text-align: left;
+		margin: 0;
 
-    h2 {
-      text-align: initial;
-      font-size: 3.6rem;
-    }
+		h2 {
+			text-align: initial;
+			font-size: 3.6rem;
+		}
 
-    img {
-      height: 100%;
-    }
-  }
+		img {
+			height: 100%;
+		}
+	}
 `
 
 export const ButtonsContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  margin: 3.2rem 0;
+	display: flex;
+	justify-content: center;
+	margin: 3.2rem 0;
 
-  @media (min-width: 1100px) {
-    grid-area: buttons;
-    justify-content: flex-start;
+	@media (min-width: 1100px) {
+		grid-area: buttons;
+		justify-content: flex-start;
 
-    a {
-      font-size: 2.4rem;
-    }
-  }
+		a {
+			font-size: 2.4rem;
+		}
+	}
 `
 
 export const Button: any = styled.div`
-  border-radius: 0.8rem;
-  
-  a {
-    width: 21rem;
-    height: 9.4rem;
-    font: 700 2.0rem Archivo;
+	border-radius: 0.8rem;
 
-    display: flex;
-    align-items: center;
-    justify-content: space-evenly;
-    flex-direction: column;
+	a {
+		width: 21rem;
+		height: 9.4rem;
+		font: 700 2rem Archivo;
 
-    text-decoration: none;
-    color: var(--color-button-text);
+		display: flex;
+		align-items: center;
+		justify-content: space-evenly;
+		flex-direction: column;
 
-    transition: background-color 0.2s;
+		text-decoration: none;
+		color: var(--color-button-text);
 
-    img {
-      width: 4rem;
-    }
-  }
-  
-  @media (min-width: 1100px){
-    a {
-      width: 30rem;
-      height: 10.4rem;
-      font-size: 2.4rem;
+		transition: background-color 0.2s;
 
-      flex-direction: row;
-      justify-content: center;
-    }
-  }
+		img {
+			width: 4rem;
+		}
+	}
 
-  ${(props: any) => {
-    if (props.study) {
-      return (css`
-        background: var(--color-primary-lighter);
+	@media (min-width: 1100px) {
+		a {
+			width: 30rem;
+			height: 10.4rem;
+			font-size: 2.4rem;
 
-        :hover{
-          background: var(--color-primary-light);
-        }
-      `)
-    }
-    if (props.give_classes) {
-      return (css`
-      background: var(--color-secundary);
+			flex-direction: row;
+			justify-content: center;
+		}
+	}
 
-      :hover{
-        background: var(--color-secundary-dark);
-      }
-    `)
-    }
+	${(props: any) => {
+		if (props.study) {
+			return css`
+				background: var(--color-primary-lighter);
 
-  }}
-  :first-child {
-    margin-right: 1.6rem;
-  }
+				:hover {
+					background: var(--color-primary-light);
+				}
+			`
+		}
+		if (props.give_classes) {
+			return css`
+				background: var(--color-secundary);
 
+				:hover {
+					background: var(--color-secundary-dark);
+				}
+			`
+		}
+	}}
+	:first-child {
+		margin-right: 1.6rem;
+	}
 `
 
 export const Span = styled.span`
-  font-size: 1.4rem;
-  width: 100%;
+	font-size: 1.4rem;
+	width: 100%;
 
-  display: flex;
-  align-items: center;
-  justify-content: center;
+	display: flex;
+	align-items: center;
+	justify-content: center;
 
-  img {
-    margin-left: 0.8rem;
-  }
+	img {
+		margin-left: 0.8rem;
+	}
 
-  @media (min-width: 1100px) {
-    grid-area: total;
-    justify-self: end;
-  }
+	@media (min-width: 1100px) {
+		grid-area: total;
+		justify-self: end;
+	}
 `
