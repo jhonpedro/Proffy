94232f37c5e947806f7f35bb53b114a6951c3004
João Pedro <pedrobarros2010@gmail.com>
2020-12-15 23:26:38 -0300

    removing push and using goBack instead

--- web/src/components/PageHeader/index.tsx
+++ web/src/components/PageHeader/index.tsx
@@ -1,41 +1,46 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
 
 import logoImg from '../../assets/images/logo.svg'
 import backIcon from '../../assets/images/icons/back.svg'
 
 // import './styles.css'
 
-import {
-  PageHeaderElement,
-  TopBar,
-  HeaderContent
-} from './styles'
+import { PageHeaderElement, TopBar, HeaderContent } from './styles'
 
 interface PageHeaderProps {
-  title: string,
-  description?: string,
-  children?: React.ReactChild
+	title: string
+	description?: string
+	children?: React.ReactChild
 }
 
-const PageHeader: React.FC<PageHeaderProps> = ({ title, description, children: Component }) => {
-  return (
-    <PageHeaderElement>
-      <TopBar>
-        <Link to="/" >
-          <img src={backIcon} alt="Voltar" />
-        </Link>
-        <img src={logoImg} alt="Logo Proffy" />
-      </TopBar>
-      <HeaderContent>
-        <strong>{title}</strong>
-        {description && (
-          <p>{description}</p>
-        )}
-        {Component}
-      </HeaderContent>
-    </PageHeaderElement>
-  )
+const PageHeader: React.FC<PageHeaderProps> = ({
+	title,
+	description,
+	children: Component,
+}) => {
+	const { goBack } = useHistory()
+
+	function handleGoBack() {
+		goBack()
+		return
+	}
+
+	return (
+		<PageHeaderElement>
+			<TopBar>
+				<span onClick={handleGoBack}>
+					<img src={backIcon} alt='Voltar' />
+				</span>
+				<img src={logoImg} alt='Logo Proffy' />
+			</TopBar>
+			<HeaderContent>
+				<strong>{title}</strong>
+				{description && <p>{description}</p>}
+				{Component}
+			</HeaderContent>
+		</PageHeaderElement>
+	)
 }
 
 export default PageHeader
--- web/src/components/PageHeader/styles.ts
+++ web/src/components/PageHeader/styles.ts
@@ -1,75 +1,75 @@
 import styled from 'styled-components'
 
 export const PageHeaderElement = styled.header`
-  display: flex;
-  flex-direction: column;
-  background-color: var(--color-primary);
+	display: flex;
+	flex-direction: column;
+	background-color: var(--color-primary);
 
-  @media (min-width: 1100px) {
-    height: 340px;
-  }
-  
+	@media (min-width: 1100px) {
+		height: 340px;
+	}
 `
 
 export const TopBar = styled.div`
-  width: 90%;
-  margin: 0 auto;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  color: var(--color-text-in-primary);
-  padding: 1.6rem 0;
+	width: 90%;
+	margin: 0 auto;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	color: var(--color-text-in-primary);
+	padding: 1.6rem 0;
+	cursor: pointer;
 
-  a {
-    height: 3.2rem;
-    transition: opacity 0.2s;
-  }
+	span {
+		height: 3.2rem;
+		transition: opacity 0.2s;
+	}
 
-  a:hover {
-    opacity: 0.6;
-  }
+	span:hover {
+		opacity: 0.6;
+	}
 
-  > img {
-    height: 1.6rem;
-  }
+	> img {
+		height: 1.6rem;
+	}
 
-  @media (min-width: 1100px) {
-    max-width: 1100px;
-  }
+	@media (min-width: 1100px) {
+		max-width: 1100px;
+	}
 `
 
 export const HeaderContent = styled.div`
-  width: 90%;
-  margin: 0 auto;
-  position: relative;
-  margin: 3.2rem auto;
+	width: 90%;
+	margin: 0 auto;
+	position: relative;
+	margin: 3.2rem auto;
 
-  p {
-    max-width: 30rem;
-    font-size: 1.6rem;
-    line-height: 2.6rem;
-    color: var(--color-text-in-primary);
-    margin-top: 2.4rem;
-  }
+	p {
+		max-width: 30rem;
+		font-size: 1.6rem;
+		line-height: 2.6rem;
+		color: var(--color-text-in-primary);
+		margin-top: 2.4rem;
+	}
 
-  strong {
-    font: 700 3.6rem Archivo;
-    line-height: 4.2rem;
-    color: var(--color-title-in-primary);
-  }
+	strong {
+		font: 700 3.6rem Archivo;
+		line-height: 4.2rem;
+		color: var(--color-title-in-primary);
+	}
 
-  @media (min-width: 1100px) {
-    flex: 1;
-    max-width: 740px;
-    margin: 0 auto;
-    padding-bottom: 48px;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: flex-start;
-  }
+	@media (min-width: 1100px) {
+		flex: 1;
+		max-width: 740px;
+		margin: 0 auto;
+		padding-bottom: 48px;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: flex-start;
+	}
 
-  strong {
-    max-width: 350px;
-  }
+	strong {
+		max-width: 350px;
+	}
 `
