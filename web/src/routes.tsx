6c52ef1130baf92dd20eabba76e0e3a8f6e3efa0
João Pedro <pedrobarros2010@gmail.com>
2020-12-19 23:12:43 -0300

    creating new routes

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { Switch, BrowserRouter, Redirect, Route } from 'react-router-dom'
 
 import SingIn from './pages/SingIn'
 import SingUp from './pages/SingUp'
@@ -7,29 +7,37 @@ import TeacherList from './pages/TeacherList'
 import TeacherForm from './pages/TeacherForm'
 import ChangePassword from './pages/ChangePassword'
 import ForgotPassword from './pages/ForgotPassword'
+import Landing from './pages/Landing'
 
 function Routes() {
 	return (
 		<BrowserRouter>
-			<Route path="/" exact>
-				<SingIn />
-			</Route>
-			<Route path="/sing-up" exact>
-				<SingUp />
-			</Route>
-			<Route path="/forgot-password">
-				<ForgotPassword />
-			</Route>
-			<Route path="/change-password/:token">
-				<ChangePassword />
-			</Route>
-
-			<Route path="/study" exact>
-				<TeacherList />
-			</Route>
-			<Route path="/give-classes" exact>
-				<TeacherForm />
-			</Route>
+			<Switch>
+				<Route path="/sing-in">
+					<SingIn />
+				</Route>
+				<Route path="/sing-up">
+					<SingUp />
+				</Route>
+				<Route path="/forgot-password">
+					<ForgotPassword />
+				</Route>
+				<Route path="/change-password/:token">
+					<ChangePassword />
+				</Route>
+				<Route path="/" exact>
+					<Landing />
+				</Route>
+				<Route path="/study">
+					<TeacherList />
+				</Route>
+				<Route path="/give-classes">
+					<TeacherForm />
+				</Route>
+				<Route path="*">
+					<Redirect to="/sing-in" />
+				</Route>
+			</Switch>
 		</BrowserRouter>
 	)
 }
