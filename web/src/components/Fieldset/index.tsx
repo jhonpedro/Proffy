5f1caca4ca04b098f02690b407d4a9dbe94e3f86
João Pedro <pedrobarros2010@gmail.com>
2021-01-08 19:07:53 -0300

    removing index

--- web/src/components/Fieldset/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import { Fieldset } from './style'
-
-const FormFieldset: React.FC = ({ children }) => {
-	return <Fieldset>{children}</Fieldset>
-}
-
-export default FormFieldset
--- web/src/components/Legend/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import { Legend } from './style'
-
-const FormLegend: React.FC = ({ children }) => {
-	return <Legend>{children}</Legend>
-}
-
-export default FormLegend
