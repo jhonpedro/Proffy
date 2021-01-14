b3f89fd283c7c1dc169e59b7de4bb21d99d2585c
João Pedro <pedrobarros2010@gmail.com>
2021-01-14 19:12:47 -0300

    removing padding

--- web/src/components/Fieldset/style.ts
+++ web/src/components/Fieldset/style.ts
@@ -2,13 +2,8 @@ import styled from 'styled-components'
 
 export const Fieldset = styled.fieldset`
 	border: 0;
-	padding: 0 2.4rem;
 
 	+ fieldset {
 		margin-top: 6.4rem;
-	}
-
-	@media (min-width: 1100px) {
-		padding: 0 6.4rem;
 	}
 `
