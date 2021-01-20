0aa84c8b7c7401aa00d6ae3bfc7c4df711b8435b
João Pedro <pedrobarros2010@gmail.com>
2021-01-20 16:25:34 -0300

    changing breakpoint from 1100px to 700px

--- web/src/components/Select/styles.ts
+++ web/src/components/Select/styles.ts
@@ -1,42 +1,42 @@
 import styled from 'styled-components'
 
 export const SelectBlock = styled.div`
-  position: relative;
+	position: relative;
 
-  + .select-block {
-    margin-top: 1.4rem;
-  }
+	+ .select-block {
+		margin-top: 1.4rem;
+	}
 
-  label {
-    font-size: 1.4rem;
-  }
-  @media (min-width: 1100px) {
-    + .select-block {
-      margin-top: 0;
-    }
-  }
+	label {
+		font-size: 1.4rem;
+	}
+	@media (min-width: 700px) {
+		+ .select-block {
+			margin-top: 0;
+		}
+	}
 
-  :focus-within::after {
-    width: calc(100% - 3.2rem);
-    height: 2px;
-    content: '';
-    background: var(--color-primary-light);
-    position: absolute;
-    left: 1.6rem;
-    right: 1.6rem;
-    bottom: 0;
-    border-radius: 0.8rem;
-  }
+	:focus-within::after {
+		width: calc(100% - 3.2rem);
+		height: 2px;
+		content: '';
+		background: var(--color-primary-light);
+		position: absolute;
+		left: 1.6rem;
+		right: 1.6rem;
+		bottom: 0;
+		border-radius: 0.8rem;
+	}
 `
 
 export const SelectElement = styled.select`
-  width: 100%;
-  height: 5.6rem;
-  margin-top: 0.8rem;
-  border-radius: 0.8rem;
-  background: var(--color-input-background);
-  border: 1px solid var(--color-line-in-white);
-  outline: 0;
-  padding: 0 1.6rem;
-  font: 1.6rem Archivo;
+	width: 100%;
+	height: 5.6rem;
+	margin-top: 0.8rem;
+	border-radius: 0.8rem;
+	background: var(--color-input-background);
+	border: 1px solid var(--color-line-in-white);
+	outline: 0;
+	padding: 0 1.6rem;
+	font: 1.6rem Archivo;
 `
--- web/src/pages/TeacherList/styles.ts
+++ web/src/pages/TeacherList/styles.ts
@@ -1,58 +1,58 @@
 import styled from 'styled-components'
 
 export const PageTeacherList = styled.div`
-  width: 100vw;
-  height: 100vh;
+	width: 100vw;
+	height: 100vh;
 
-  max-width: 100%;
-  @media(min-width: 1100px) {
-  }
+	max-width: 100%;
+	@media (min-width: 700px) {
+	}
 `
 
 export const Form = styled.form`
-  margin-top: 3.2rem;
+	margin-top: 3.2rem;
 
-  label {
-    color: var(--color-text-in-primary);
-  }
+	label {
+		color: var(--color-text-in-primary);
+	}
 
-  button {
-    width: 100%;
-    height: 5.6rem;
-    background: var(--color-secundary);
-    color: var(--color-button-text);
-    border: 0;
-    border-radius: 0.8rem;
-    cursor: pointer;
-    font: 700 1.6rem Archivo;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    text-decoration: none;
-    transition: background-color 0.2s;
-    margin-top: 3.2rem;
-  }
+	button {
+		width: 100%;
+		height: 5.6rem;
+		background: var(--color-secundary);
+		color: var(--color-button-text);
+		border: 0;
+		border-radius: 0.8rem;
+		cursor: pointer;
+		font: 700 1.6rem Archivo;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		text-decoration: none;
+		transition: background-color 0.2s;
+		margin-top: 3.2rem;
+	}
 
-  button:hover {
-    background: var(--color-secundary-dark);
-  }
+	button:hover {
+		background: var(--color-secundary-dark);
+	}
 
-  @media (min-width: 1100px) {
-    display: grid;
-    grid-template-columns: 1.33fr 1.33fr 1.33fr 0.5fr;
-    column-gap: 16px;
-    position: absolute;
-    bottom: -28px;
+	@media (min-width: 700px) {
+		display: grid;
+		grid-template-columns: 1.33fr 1.33fr 1.33fr 0.5fr;
+		column-gap: 16px;
+		position: absolute;
+		bottom: -28px;
 
-    button {
-    }
-  }
+		button {
+		}
+	}
 `
 
 export const Main = styled.main`
-  @media (min-width: 1100px) {
-    padding: 3.2rem 0;
-    max-width: 740px;
-    margin: 0 auto;
-  }
+	@media (min-width: 700px) {
+		padding: 3.2rem 0;
+		max-width: 740px;
+		margin: 0 auto;
+	}
 `
