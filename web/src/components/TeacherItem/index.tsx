96b9c43dac6fdce42252a2b9448fe3158be4e302
João Pedro <pedrobarros2010@gmail.com>
2021-01-27 23:21:42 -0300

    adding schedules to TeacherItem

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -1,68 +1,139 @@
-import React from 'react'
-import api from '../../services/api'
+import React, { useState } from 'react'
+import api from '../../services/axios'
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 
 // import './styles.css'
 
 import {
-  TeacherItemContainer,
-  Header,
-  Paragraph,
-  Footer
+	TeacherItemContainer,
+	Header,
+	Paragraph,
+	Footer,
+	SchedulesContainer,
+	ScheduleItem,
 } from './styles'
 
 export interface Teacher {
-  id: number
-  name: string
-  avatar: string
-  whatsapp: string
-  bio: string
-  subject: string
-  cost: number
+	id: number
+	name: string
+	avatar: string
+	whatsapp: string
+	bio: string
+	subject: string
+	cost: number
 }
 
 interface TeacherItemProps {
-  teacher: Teacher
+	teacher: Teacher
+}
+
+interface Schedule {
+	week_day: number
+	start: number
+	end: number
 }
 
 const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+	const scheduleTest = [
+		{
+			week_day: 0,
+			start: 8,
+			end: 12,
+		},
+	]
+	const [scheduleDays, setSchedulesDays] = useState<Array<Schedule>>(() => {
+		const days = new Array()
 
-  function createNewConnection() {
-    api.post('/connections', {
-      user_id: teacher.id
-    })
-  }
+		for (let i = 0; i < 7; i++) {
+			if (scheduleTest[i]) {
+				if (scheduleTest[i].week_day == i) {
+					days.push(scheduleTest[i])
+					continue
+				}
+			}
+			days.push([{}])
+		}
 
-  return (
-    <TeacherItemContainer>
-      <Header>
-        <img src={teacher.avatar} alt={teacher.name} />
-        <div>
-          <strong>{teacher.name}</strong>
-          <span>{teacher.subject}</span>
-        </div>
-      </Header>
-      <Paragraph>
-        {teacher.bio}
-      </Paragraph>
-      <Footer>
-        <p>
-          Preço/Hora
-          <strong>R$ {teacher.cost}</strong>
-        </p>
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href={`https://api.whatsapp.com/send?phone=${teacher.whatsapp}`}
-          onClick={createNewConnection}
-        >
-          <img src={whatsappIcon} alt="WhatsApp" />
-          Entrar em contato
-        </a>
-      </Footer>
-    </TeacherItemContainer>
-  )
+		return days
+	})
+
+	function getDayOfWeekBasedOnNumberGiven(number: number): string {
+		switch (number) {
+			case 0:
+				return 'Segunda-feira'
+			case 1:
+				return 'Terça-feira'
+			case 2:
+				return 'Quarta-feira'
+			case 3:
+				return 'Quinta-feira'
+			case 4:
+				return 'Sexta-feira'
+			case 5:
+				return 'Sábado'
+			case 6:
+				return 'Domingo'
+			default:
+				return 'Dia inexistente'
+		}
+	}
+
+	function createNewConnection() {
+		api.post('/connections', {
+			user_id: teacher.id,
+		})
+	}
+
+	return (
+		<TeacherItemContainer>
+			<Header>
+				<img src={teacher.avatar} alt={teacher.name} />
+				<div>
+					<strong>{teacher.name}</strong>
+					<span>{teacher.subject}</span>
+				</div>
+			</Header>
+			<Paragraph>{teacher.bio}</Paragraph>
+			<SchedulesContainer>
+				<div className='schedule-strong-box'></div>
+				{scheduleDays.map((day, index) => {
+					return (
+						<ScheduleItem
+							key={index}
+							hasTime={day.start & day.end ? true : false}
+						>
+							<strong className='week_day'>
+								{getDayOfWeekBasedOnNumberGiven(index)}
+							</strong>
+							<strong className='time'>
+								{day.start & day.end
+									? `${day.start ? day.start + 'h' : ''} - ${
+											day.end ? day.end + 'h' : ''
+									  }`
+									: 'Sem horário'}
+							</strong>
+						</ScheduleItem>
+					)
+				})}
+			</SchedulesContainer>
+			<Footer>
+				<p>
+					Preço/Hora
+					<strong>R$ {teacher.cost}</strong>
+				</p>
+				<a
+					target='_blank'
+					rel='noopener noreferrer'
+					href={`https://api.whatsapp.com/send?phone=${teacher.whatsapp}`}
+					onClick={createNewConnection}
+				>
+					<img src={whatsappIcon} alt='WhatsApp' />
+					Entrar em contato
+				</a>
+			</Footer>
+		</TeacherItemContainer>
+	)
 }
 
 export default TeacherItem
--- web/src/components/TeacherItem/styles.ts
+++ web/src/components/TeacherItem/styles.ts
@@ -1,10 +1,13 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+
+interface ScheduleItemProps {
+	hasTime: boolean
+}
 
 export const TeacherItemContainer = styled.article`
 	background: var(--color-box-base);
 	border: 1px solid var(--color-line-in-white);
 	border-radius: 0.8rem;
-	margin-top: 2.4rem;
 	overflow: hidden;
 
 	> p {
@@ -14,8 +17,6 @@ export const TeacherItemContainer = styled.article`
 	}
 
 	@media (min-width: 700px) {
-		padding: 0 3.2rem;
-
 		> p {
 			padding: 0 3.2rem;
 		}
@@ -61,11 +62,52 @@ export const Paragraph = styled.p`
 	line-height: 2.8rem;
 `
 
+export const SchedulesContainer = styled.div`
+	padding: 0.5rem 2rem;
+
+	.schedule-strong-box {
+		display: flex;
+		justify-content: space-between;
+		font-size: 1.2rem;
+		padding: 0 2rem;
+
+		::before {
+			content: 'Dia';
+			display: inline-block;
+		}
+		::after {
+			content: 'Horário';
+			display: inline-block;
+		}
+	}
+`
+
+export const ScheduleItem = styled.div<ScheduleItemProps>`
+	display: flex;
+	justify-content: space-between;
+	font-size: 1.6rem;
+	padding: 1rem;
+	background-color: var(--color-box-footer);
+	border: 2px solid var(--color-line-in-white);
+	border-radius: 2rem;
+	margin-bottom: 1rem;
+
+	${(props) => {
+		if (!props.hasTime) {
+			return css`
+				filter: opacity(60%);
+			`
+		}
+
+		return
+	}}
+`
+
 export const Footer = styled.footer`
 	padding: 3.2rem;
 	background: var(--color-box-footer);
 	border-top: 1px solid var(--color-line-in-white);
-	margin-top: 3.2rem;
+	margin-top: 1.5rem;
 
 	display: flex;
 	align-items: center;
