f363a3404805a0092a7f5ecf17ebc5c71dbe432c
João Pedro <pedrobarros2010@gmail.com>
2021-02-13 20:09:15 -0300

    fix: testing db and running migrations before server starts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,12 @@
 import App from './app'
 
+import testDatabaseConnection from './utils/helpers/testDatabaseConnection'
+;(async () => {
+	if (await !testDatabaseConnection()) {
+		throw new Error('Database is not connected')
+	}
+})()
+
 App.listen(3030, () =>
 	console.log('Server is running on http://localhost:3030')
 )
